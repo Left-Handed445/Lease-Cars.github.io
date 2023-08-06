5949c647d5bf9b8f2d56bad71cf5aa48a2e65d76
Left-Handed <workout2020@list.ru>
2023-08-06 12:09:48 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+const tabItem = document.querySelectorAll(".tabs__btn-item");
+const tabContent = document.querySelectorAll(".tabs__content-item");
+
+tabItem.forEach(function (element) {
+  element.addEventListener("click", open);
+});
+
+function open(event) {
+  const tabTarget = event.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach(function (item) {
+    item.classList.remove("tabs__btn-item-active");
+  });
+
+  tabTarget.classList.add("tabs__btn-item-active");
+
+  tabContent.forEach(function (item) {
+    item.classList.remove("tabs__content-item-active");
+  });
+
+  document
+    .querySelector(`#${button}`)
+    .classList.add("tabs__content-item-active");
+}
+
+const menuBtn = document.querySelector(".menu__btn");
+const menu = document.querySelector(".menu__list");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("menu__list-active");
+});
+
+const swiper = new Swiper(".mySwiper", {
+  effect: "fade",
+  autoplay: {
+    delay: 3000,
+    disableOneInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
